a4e23a5b3e73379b37f0bfdcb26377f88670bb5f
Elnatan Samuel <elnatansamuel25@gmail.com>
2025-10-15 09:55:54 +0300

    yes

--- src/utils/authClient.ts
+++ src/utils/authClient.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from 'better-auth/react';
 
-const baseURL = process.env.NEXT_PUBLIC_AUTH_URL || 'http://localhost:4000';
+const baseURL = process.env.NEXT_PUBLIC_BETTER_AUTH_URL || 'http://localhost:4000';
 
 const client = createAuthClient({ baseURL });
 
